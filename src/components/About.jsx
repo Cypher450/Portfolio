cda0d194bf3c380acc86ee599172bdb67eef75f4
Cypher450 <seshdev01012001@gmail.com>
2023-08-18 21:37:10 +0530

    corected the name

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
                 </div>
 
                 <p className='text-xl mt-20'>
-                Hi, I'm Seshaev Sahoo, a fourth-year BTech student at the Silicon Institute of Technology, Bhubaneswar. I'm passionate about using technology to solve problems and make a positive impact on the world. I'm Familiar in web development technologies: HTML, CSS, JavaScript, Node.js, Express, and MongoDB.. I'm also interested in artificial intelligence, machine learning, and data science. I'm excited to be a part of the AI revolution and to use my skills to make a difference in the world.
+                Hi, I'm Seshadev Sahoo, a fourth-year BTech student at the Silicon Institute of Technology, Bhubaneswar. I'm passionate about using technology to solve problems and make a positive impact on the world. I'm Familiar in web development technologies: HTML, CSS, JavaScript, Node.js, Express, and MongoDB.. I'm also interested in artificial intelligence, machine learning, and data science. I'm excited to be a part of the AI revolution and to use my skills to make a difference in the world.
                 </p>
                 <br />
                 <p className='text-xl'>I did my Class 12th from DPS Kalinga with 78% and my class 10th from the same school with 9.4 cgpa. Some of my hobbies are like football, martial arts and i also have an interest in world politics so i watch some geopolitics videos time to time on youtube.
